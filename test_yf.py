7e2cb57c56de45dd58240afb0996422b8b8be59d
Vinay Naik <vinay2522@vinaynaik252@gmail.com>
2025-11-25 21:51:25 +0530

    Add .gitignore; exclude venv, DB, staticfiles, tests, and debug scripts

--- test_yf.py
+++
@@ -1,6 +0,0 @@
-﻿import yfinance as yf
-import pandas as pd
-df = yf.download('TSLA', period='5d', interval='1d', progress=False, auto_adjust=True)
-print('Columns:', list(df.columns))
-print('Shape:', df.shape)
-print(df)
